e1c29a3c0b0f702a0ebfabc52f47163b141817b7
MOSORIOLO <mariana.osorio27@hotmail.com>
2024-03-08 20:56:56 -0500

    feat: :white_check_mark: subiendo cambios

--- cuenta.php
+++ cuenta.php
@@ -3,11 +3,16 @@
 
 class cuenta
 {
-    
+    private $nombre;
+    private$numCuenta;
+    private $tipoInteres;
+    private $saldo;
+
+    public function __construct($nombre,$numCuenta,$tipoInteres, $saldo)
+    {
+        $this-> nombre =$nombre;
+        $this-> numCuenta =$numCuenta;
+        $this-> tipoInteres = $tipoInteres;
+        $this->saldo=$saldo;
+    }
 }
-
-
-
-
-
-?>
